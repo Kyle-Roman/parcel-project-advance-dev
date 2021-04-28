343acad8861b7f20ff349b5a51ce4f922a7a8276
Nika Prokopeva <nika.pro282@gmail.com>
2021-04-28 15:19:21 +0200

    add smooth scroll to anchors

--- src/anchor-scroll.js
+++ src/anchor-scroll.js
@@ -20,11 +20,22 @@
 
 
 
- $("a.scrollto").click(function() {
-    var elementClick = $(this).attr("href")
-    var destination = $(elementClick).offset().top;
-    jQuery("html:not(:animated),body:not(:animated)").animate({
-        scrollTop: destination
-    }, 500);
-    return false;
- });
+//  $("a.scrollto").click(function() {
+//     var elementClick = $(this).attr("href")
+//     var destination = $(elementClick).offset().top;
+//     jQuery("html:not(:animated),body:not(:animated)").animate({
+//         scrollTop: destination
+//     }, 500);
+//     return false;
+//  });
+
+var $page = $('html, body');
+$('a[href*="#"]').click(function () {
+  $page.animate(
+    {
+      scrollTop: $($.attr(this, 'href')).offset().top,
+    },
+    900,
+  );
+  return false;
+});
